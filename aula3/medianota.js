281c56cbd2315c55445babe53b7688a6ebf3d804
lhassis <assis.luish@gmail.com>
2025-08-14 15:11:51 -0300

    segundo commit

---
+++ aula3/medianota.js
@@ -0,0 +1,23 @@
+//nota aluno
+let n1 = 7
+let n2 = 5
+let n3 = 1
+let n4 = 10
+//mdeia do aluno
+let media = (n1+n2+n3+n4)/4
+//mostrar a media 
+console.log(media)
+
+let numero = Math.sqrt(9)
+
+let a = 2
+let b = 8
+let c = 9
+
+let delta = (b**b-4*a*c)
+
+let x1 = (-b + Math.sqrt{(b**-2*a*c)})/2*a
+let x2 = (-b - Math.sqrt{(b**-2*a*c)})/2*a
+
+console.log(x1)
+console.log(x2)
