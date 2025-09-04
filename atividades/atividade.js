0f1a889662f1d4326871ff30008a5b79aeac07ec
jv calvo <jv4545666@gmail.com>
2025-09-04 16:28:52 -0300

    Atividade 04/09

---
+++ atividades/atividade.js
@@ -0,0 +1,18 @@
+// Simples: nçao pode ser subdividido em partes.
+// Composto: Pode ser decomposto em subatributos.
+// Monovalorado: Só aceita um valor por vez.
+// Multivalorado: Pode ter mais de um valor ao mesmo tempo.
+
+
+// Nome: Simples, Monovalorado
+// CPF: Simples, Monovalorado
+// Telefone: Simples, Multivalorado
+// Matricula: Simples, Monovalorado
+// Idade: Simples, Monovalorado
+// Endereço: Composto, Mono
+// Rua: Simples, Monovalorado
+// Bairro: Simples, Monovalorado
+// CEP: Simples, Monovalorado
+// Cidade: Simples, Monovalorado
+// Estado: Simples, Monovalorado
+// Codigo: Simples, Monovalorado
---
+++ aula8/aula8.ts
@@ -0,0 +1,17 @@
+// Classe para Corvo
+class Corvo {
+    private animal: string
+    public plumagem: string
+    public inteligencia: string
+    public especie: string
+    public ave: string
+    public tamanho: number
+
+    Voando(){
+        
+    }
+
+    Pouso(){
+        
+    }
+}
